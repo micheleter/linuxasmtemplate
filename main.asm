d6e97ddc5e4e3ce06fcfa4c22e98e20a8b980e29
Chase Zimmerman <chase@thezimmerman.net>
2018-02-07 20:42:21 -0800

    Initial commit for 64bit version

--- main.asm
+++ main.asm
@@ -1,19 +1,20 @@
-bits 32
 section .data
-  ;Initialized data definitions go here
 
+  ;Initialized data definitions go here
 section .bss
+
   ;Uninitialized memory reservations go here
 
 section .text
-
 global _start
 _start:
   nop
-  ;Code starts here
+  ; Put program code here
+  ; End program code here
 
-  ;Code ends here
   nop
-  mov eax,1 ; Exit system call value
-  mov ebx,0 ; Exit return code
-  int 80h ; Call the kernel
+
+; Exit the program Linux Legally!
+mov eax,1 ; Exit system call value
+mov ebx,0 ; Exit return code
+int 80h ; Call the kernel
